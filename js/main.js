bede125448acec56875b0311769958ce58001dd3
Sam Humphreys <s-humphreys@hotmail.com>
2016-05-23 01:18:55 +1000

    I seem to have updated something since the last commit

--- js/main.js
+++ js/main.js
@@ -3,14 +3,11 @@ $(document).ready(function () {
   //keep navbar at the top of the page
   $(window).on('scroll', function () {
     var heightToTop = $(document).scrollTop();
-    console.log(heightToTop);
     if (heightToTop >= 20) {
       $('.navbar').addClass('shrunken');
-      // $('.navbar').addClass('on-the-side');
       $('.to-top').show();
     } else {
       $('.navbar').removeClass('shrunken');
-      // $('.navbar').removeClass('on-the-side');
       $('.to-top').hide();
     };
   });
@@ -24,11 +21,11 @@ $(document).ready(function () {
     var waitASec = function () {
       setTimeout(function () {
         pause1.resolve();
-      // },1000);
-      },10);                  //change to 1000
+      },1000);
     };
 
     var typeTheHeading = function () {
+      $('body').animate({scrollTop: 0},100);
       var phrase = "console.log('Hi, I'm Sam');".split('');
       var x = 0;
       var y = phrase.length;
@@ -41,19 +38,19 @@ $(document).ready(function () {
           window.clearInterval(typeItOut);
           setTimeout(function () {
             pause2.resolve();
-          // }, 2000);
-          }, 20);
+          }, 2000);
         };
-      // }, 125);
-      }, 1);
+      }, 125);
     };
 
     var removeTypedHeading = function () {
-      $('.typed-heading').fadeOut(15, addContent);   //change to 1500
+      $('.typed-heading').fadeOut(1500, addContent);
     };
 
     var addContent = function () {
-      $('.content').fadeIn(15);    //change to 1500
+      $('.navbar').fadeIn(1500);
+      $('.content').fadeIn(1500);
+      $('footer').show();
       clickListeners.addAll();
     };
 
